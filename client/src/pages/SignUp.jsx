8d0eb00e5c3007c5450275a5780f1b56d12267a3
MDX2002 <143632411+MDX2002@users.noreply.github.com>
2025-07-19 00:19:23 +0530

    some db error handling + Sign Up UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+
+      <form className="flex flex-col gap-4">
+        <input type="text" placeholder="Username" className="border p-3 rounded-lg" id="username" />
+        <input type="email" placeholder="Email" className="border p-3 rounded-lg" id="email" />
+        <input type="password" placeholder="Password" className="border p-3 rounded-lg" id="password" />
         
-        SignUp
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Sign Up
+        </button>
+      </form>
+
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <span className="text-blue-700">sign In</span>
+      </div>
+
     </div>
   )
 }
